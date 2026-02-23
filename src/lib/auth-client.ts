94951a244814bbf515a58e674fdc4e64f158b26e
Manish <manishvishwakarma324@gmail.com>
2026-02-23 15:24:10 +0530

    refactor: fix minor bug

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,5 +1,6 @@
 import { createAuthClient } from "better-auth/react";
-import { env } from "process";
+import { env } from "./env";
+
 export const authClient = createAuthClient({
   baseURL: env.BETTER_AUTH_BASE_URL,
 });
