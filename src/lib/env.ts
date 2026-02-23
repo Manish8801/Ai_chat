af9f2ab01bac7b40de7ee77e2a1eda519b6df2b8
Manish <manishvishwakarma324@gmail.com>
2026-02-23 15:02:28 +0530

    refactor: add deployment time script

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -9,9 +9,7 @@ function getEnv(key: string) {
 
 export const env = {
   BETTER_AUTH_SECRET: getEnv("BETTER_AUTH_SECRET"),
-  DATABASE_URL:
-    getEnv("DATABASE_URL") ||
-    `postgresql://${getEnv("POSTGRES_USER")}:${getEnv("POSTGRES_PASSWORD")}@localhost:5433/${getEnv("POSTGRES_DB")}`,
+  DATABASE_URL: getEnv("DATABASE_URL"),
   GITHUB_CLIENT_ID: getEnv("GITHUB_CLIENT_ID"),
   GITHUB_CLIENT_SECRET: getEnv("GITHUB_CLIENT_SECRET"),
   GOOGLE_CLIENT_ID: getEnv("GOOGLE_CLIENT_ID"),
