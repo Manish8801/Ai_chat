8e752b0d8ca916033d3468016f5226de3bb99554
Manish <manishvishwakarma324@gmail.com>
2026-02-24 12:20:16 +0530

    feat(UI): redesign mode toggle for improved usabilit and add heading in home pagey

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -6,7 +6,8 @@ export default async function HomePage() {
   const session = await getSession();
 
   return (
-    <div className="h-screen w-full flex items-center justify-center ">
+    <div className="h-screen w-full flex flex-col items-center justify-center gap-10">
+      <h1 className=" ">Free AI Chatbot to chat with.</h1>
       <Button asChild>
         <Link href={session ? "/chats" : "/sign-up"}>Get Started</Link>
       </Button>
--- src/features/root/components/mode-toggle.tsx
+++ src/features/root/components/mode-toggle.tsx
@@ -1,37 +1,41 @@
 "use client";
 import { Button } from "@/components/ui/button";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-import { Moon, Sun } from "lucide-react";
+import { Laptop, Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
 
 export function ModeToggle() {
-  const { setTheme } = useTheme();
+  const { theme, setTheme } = useTheme();
+
+  const handleButton = (theme: "dark" | "light" | "system") => {
+    setTheme(theme);
+  };
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="outline" className="rounded-full ">
-          <Sun className="scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
-          <Moon className="absolute scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
-          <span className="sr-only">Toggle theme</span>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => setTheme("light")}>
-          Light
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("dark")}>
-          Dark
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("system")}>
-          System
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <div className="flex items-center justify-center rounded-full bg-accent-foreground/10">
+      <Button
+        variant="ghost"
+        size="icon"
+        className={`${theme === "light" ? "bg-accent-foreground/20" : ""} rounded-full flex items-center justify-center transition-colors duration-100`}
+        onClick={() => handleButton("light")}
+      >
+        <Sun />
+      </Button>
+      <Button
+        variant="ghost"
+        size="icon"
+        className={`${theme === "dark" ? "bg-accent-foreground/20" : ""} rounded-full flex items-center justify-center transition-colors duration-100`}
+        onClick={() => handleButton("dark")}
+      >
+        <Moon />
+      </Button>
+      <Button
+        variant="ghost"
+        size="icon"
+        className={`${theme === "system" ? "bg-accent-foreground/20" : ""} rounded-full flex items-center justify-center transition-colors duration-100`}
+        onClick={() => handleButton("system")}
+      >
+        <Laptop />
+      </Button>
+    </div>
   );
 }
